e188085d54580413855280f1d2f2d8abeddf75cf
fidelwx1992 <fidelherrera1992@gmail.com>
2019-08-31 18:12:00 -0400

    Primer commit

---
+++ src/components/WeatherLocation/index.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import Location from './Location';
+import WeatherData from './WeatherData';
+import {
+	SUN,
+	WINDY,
+} from './../../constants/weathers';
+import './styles.css';
+
+const data ={
+	temperature: 5,
+	weatherState: SUN,
+	humidity: 10,
+	wind: '10 m/s',
+
+}
+
+const data2 ={
+	temperature: 15,
+	weatherState: WINDY,
+	humidity: 20,
+	wind: '10 m/s',
+
+}
+
+class WeatherLocation extends Component{
+
+	constructor(){
+		super();
+		this.state = {
+			city: "Caracas",
+			data: data,
+		};
+	}	
+
+	handleUpdateClick = () => {
+		console.log("Actualizado");
+
+		this.setState({
+			data: data2
+		})
+	}
+
+	render(){
+		const {city, data} = this.state;
+		return(
+			<div className="weatherLocationCont">
+			<Location city={city}></Location>
+			<WeatherData data={data}></WeatherData>
+			<button onClick={this.handleUpdateClick}>Actualizar</button>
+			</div>
+		)
+	}
+
+}
+
+export default WeatherLocation; 
